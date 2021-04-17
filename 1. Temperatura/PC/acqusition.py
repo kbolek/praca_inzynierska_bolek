5afc9edc3a8e2d293b5c5299a75a71e390588c55
Krzysztof Bolek <krzysztof.bolek7@gmail.com>
2021-04-17 20:17:05 +0200

    PC softwarte version with iterator

--- 1. Temperatura/PC/acqusition.py
+++ 1. Temperatura/PC/acqusition.py
@@ -1,20 +1,30 @@
 import serial
 import serial.tools.list_ports
 import keyboard
+ 
 
-if __name__ == "__main__":    
-    try:
-        port = next((str(port)[:4] for port in list(serial.tools.list_ports.comports()) if "Arduino Micro" in str(port)),None)
-        baudrate = 9600
-        with serial.Serial(port,baudrate,timeout = 0.5) as ser:
-            while(not keyboard.is_pressed(' ')):
-                ser.write('s\n'.encode())
-                rx_data =  ser.readline().decode('utf-8').rstrip()
-                print(rx_data)   
-                
-    except Exception as exc:
-        print(exc)
-    
-
-    
+if __name__ == "__main__": 
+    ports = iter(list(serial.tools.list_ports.comports()))
+    while True:
+        try:
+            port = next(ports)
+            if "Arduino" in str(port):
+                port = str(port)[:5]
+                #port = next((str(port)[:4] for port in list(serial.tools.list_ports.comports()) if "Arduino Micro" in str(port)),None)
+            baudrate = 9600
+            with serial.Serial(port,baudrate,timeout = 0.5) as ser:
+                while(not keyboard.is_pressed(' ')):
+                    ser.write('s\n'.encode())
+                    rx_data =  ser.readline().decode('utf-8').rstrip()
+                    print(rx_data) 
+            break          
+        except serial.SerialException:
+            continue
+        
+        except Exception as exc:
+            print(exc)
+        break
+     
   
+    
+   
--- 1. Temperatura/uC/TempMeasureSOFT/TempMeasureSOFT.ino
+++ 1. Temperatura/uC/TempMeasureSOFT/TempMeasureSOFT.ino
@@ -1,22 +1,8 @@
-#include <Adafruit_MAX31865.h>
-
-#define SPI_CS 17
-#define SPI_MOSI 16
-#define SPI_MISO 14
-#define SPI_CLK 15
-
-#define RREF      430.0
-#define RNOMINAL  100.0
-
-Adafruit_MAX31865 * thermo;
-
 void setup() {
   Serial.begin(9600);
-  thermo = new Adafruit_MAX31865(SPI_CS, SPI_MOSI, SPI_MISO, SPI_CLK);
-  thermo->begin(MAX31865_4WIRE);
 }
 
 void loop() {
   String ser = Serial.readStringUntil('\n');
-  Serial.print(String(thermo->temperature(RNOMINAL, RREF)) + '\n');   
+  Serial.print(String("test") + '\n');   
 }
