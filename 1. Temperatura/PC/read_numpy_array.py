980bb7809117c508222041f4ae656747a34e7d69
Krzysztof Bolek <krzysztof.bolek7@gmail.com>
2021-04-27 16:40:52 +0200

    Remove the try exception

--- 1. Temperatura/PC/read_numpy_array.py
+++ 1. Temperatura/PC/read_numpy_array.py
@@ -3,18 +3,17 @@ from matplotlib import pyplot as plt
 
 
 FILE_PATH = 'acquisition.npy'
-try:
-    with open(FILE_PATH,'rb') as file:
-        data = np.load(file)
+
+with open(FILE_PATH,'rb') as file:
+    data = np.load(file)
         
-        plt.title('Temperature measurement')
-        plt.ylabel('Temperature (Celsius)')
-        plt.xlabel('Time (seconds)')
-        plt.plot(data[0],data[1])
-        plt.show()
- 
-except Exception as exc:
-        print(exc)        
+    plt.title('Temperature measurement')
+    plt.ylabel('Temperature (Celsius)')
+    plt.xlabel('Time (seconds)')
+    plt.plot(data[0],data[1])
+    plt.show()    
+
+                
            
          
      
