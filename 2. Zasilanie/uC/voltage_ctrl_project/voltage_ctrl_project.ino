f8fb86e027c98137b993494390941ee5e9985f2e
Krzysztof Bolek <krzysztof.bolek7@gmail.com>
2021-08-01 21:02:20 +0200

    Add the handling the command function

--- 2. Zasilanie/uC/voltage_ctrl_project/voltage_ctrl_project.ino
+++ 2. Zasilanie/uC/voltage_ctrl_project/voltage_ctrl_project.ino
@@ -5,12 +5,30 @@ void setup(void)
 {
   Serial.begin(9600);
   INA219_begin();
-  callib_voltage(10);
-  callib_current(100);
 }
 
 void loop(void) 
 {
-  String serial = Serial.readStringUntil('\n');
-  Serial.print(String(read_current(),10) + '\n');
-}
+    String serial = Serial.readStringUntil('\n');
+    if(serial.substring(0,3) == "rvr"){
+      Serial.print(String(read_voltage_reg(),10) + '\n');
+    }
+    else if(serial.substring(0,2) == "cc"){
+      callib_current(serial.substring(3,6).toInt());
+      Serial.print("Current callibrated");
+    }
+    else if(serial.substring(0,2) == "cv"){
+      callib_voltage(serial.substring(3,6).toInt());
+      Serial.print("Voltage callibrated");
+    }
+    else if(serial.substring(0,2) == "rc"){
+      Serial.print(String(read_current(),10) + '\n');
+    }
+    else if(serial.substring(0,2) == "rv"){
+      Serial.print(String(read_voltage(),10) + '\n');
+    }
+     
+  }
+
+
+
